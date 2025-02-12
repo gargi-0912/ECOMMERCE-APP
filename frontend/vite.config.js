5b176b6053829cf514f3bec1096c9f9f82df4162
gargi-0912 <140802693+gargi-0912@users.noreply.github.com>
2025-02-12 18:46:20 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server:{port:5173}
+// })
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{port:5173}
-})
+  server: {
+    host: "0.0.0.0",  // Ensure it listens on all network interfaces
+    port: process.env.PORT || 5173, // Use the dynamically assigned port
+  },
+});
